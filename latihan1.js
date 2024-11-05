0da07e66ae58c3cd1cc93527c7fdb1332b72b818
DikiKewoy12 <diki.krnia10@gmail.com>
2024-11-05 20:53:05 +0700

    sudah menyelesaikan tugas

---
+++ latihan1.js
@@ -0,0 +1,8 @@
+const angka = [1, 1, 2, 2, 2, 3, 3, 3, 3];
+
+const Angka = angka.reduce((frequency, num) => {
+    frequency[num] = (frequency[num] || 0) + 1; 
+    return frequency; 
+}, {});
+
+console.log(Angka);
